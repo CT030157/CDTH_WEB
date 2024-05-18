85b5c6839c480fb823283d0488cb31f772a66090
CT030157 <167410910+CT030157@users.noreply.github.com>
2024-05-18 23:06:54 +0700

    test cors++

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,5 +1,5 @@
-export const USER_SERVER = 'https://cdth-web.vercel.app/api/users';
-export const PRODUCT_SERVER = 'https://cdth-web.vercel.app/api/products';
+// export const USER_SERVER = 'https://cdth-web.vercel.app/api/users';
+// export const PRODUCT_SERVER = 'https://cdth-web.vercel.app/api/products';
 
-// export const USER_SERVER = '/api/users';
-// export const PRODUCT_SERVER = '/api/products';
+export const USER_SERVER = '/api/users';
+export const PRODUCT_SERVER = '/api/products';
