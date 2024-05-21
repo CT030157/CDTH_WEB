dbb39bd92b407ba519ee2674e8ce75c97beaa2f1
CT030157 <167410910+CT030157@users.noreply.github.com>
2024-05-21 22:41:53 +0700

    bearer token

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,32 @@
 const { User } = require('../models/User');
 
 let auth = (req, res, next) => {
-  let token = req.cookies.w_auth;
+  let token;
+  let tokenCookie = req.cookies.w_auth;
+
+  if (tokenCookie) {
+    token = tokenCookie
+  } else {
+    let authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      token = ''
+    } else {
+      let parts = authHeader.split(' ');
+  
+      if (!parts.length === 2) {
+        token = ''
+      } else {
+        let [ scheme, tokenBearer ] = parts;
+    
+        if (!/^Bearer$/i.test(scheme)) {
+          token = ''
+        } else {
+          token = tokenBearer
+        }
+      }
+    }
+  }
 
   User.findByToken(token, (err, user) => {
     if (err) throw err;
