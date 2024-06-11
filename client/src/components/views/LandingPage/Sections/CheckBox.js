4bc27559f74fb640319e117884aa588182210ff6
CT030157 <167410910+CT030157@users.noreply.github.com>
2024-06-11 08:16:15 +0700

    fix

--- client/src/components/views/LandingPage/Sections/CheckBox.js
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -3,6 +3,13 @@ import { Checkbox, Collapse } from 'antd';
 
 const { Panel } = Collapse
 
+const categories = [
+    { key: 1, value: "Áo Nam", gender: 1, group: 1 },
+    { key: 2, value: "Quần Nam", gender: 1, group: 2 },
+    { key: 3, value: "Áo Nữ", gender: 2, group: 1 },
+    { key: 4, value: "Quần Nữ", gender: 2, group: 2 },
+    { key: 5, value: "Phụ Kiện", gender: 3, group: 1 },
+]
 
 function CheckBox(props) {
 
@@ -24,22 +31,34 @@ function CheckBox(props) {
 
     }
 
-    const renderCheckboxLists = () => props.list && props.list.map((value, index) => (
-        <React.Fragment key={index}>
-            <Checkbox
-                onChange={() => handleToggle(value._id)}
-                type="checkbox"
-                checked={Checked.indexOf(value._id) === -1 ? false : true}
-            />&nbsp;&nbsp;
-            <span>{value.name}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </React.Fragment>
+    const renderCheckboxLists = (gender, group) => props.list && props.list.filter(item => item.gender == gender && item.group == group).map((value, index) => (
+        <div key={index} style={{ display: 'block' }}>
+            <React.Fragment key={index}>
+                <Checkbox
+                    onChange={() => handleToggle(value._id)}
+                    type="checkbox"
+                    checked={Checked.indexOf(value._id) === -1 ? false : true}
+                />&nbsp;&nbsp;
+                <span>{value.name}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </React.Fragment>
+        </div>
+    ))
+
+    const renderCategories = () => categories.map(({key, value, gender, group}) =>(
+        // <div style={{ display: 'inline-block', marginRight: '10px' }}>
+            <Collapse defaultActiveKey={['0']} >
+                <Panel header={value} key={key}  >
+                    {renderCheckboxLists(gender, group)}
+                </Panel>
+            </Collapse>
+        // </div>
     ))
 
     return (
         <div>
             <Collapse defaultActiveKey={['0']} >
                 <Panel header="Danh mục" key="1">
-                    {renderCheckboxLists()}
+                    {renderCategories()}
                 </Panel>
             </Collapse>
         </div>
--- client/src/components/views/LandingPage/Sections/Datas.js
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -7,28 +7,28 @@ const price = [
     },
     {
         "_id": 1,
-        "name": "1.000VNĐ to 199.000VNĐ",
-        "array": [0, 199]
+        "name": "dưới 200.000VNĐ",
+        "array": [0, 199000]
     },
     {
         "_id": 2,
-        "name": "200.000VNĐ to 499.000VNĐ",
-        "array": [200, 499]
+        "name": "200.000VNĐ đến 499.000VNĐ",
+        "array": [200000, 499000]
     },
     {
         "_id": 3,
-        "name": "500.000VNĐ to 1.999.000VNĐ",
-        "array": [500, 1999]
+        "name": "500.000VNĐ đến 1.999.000VNĐ",
+        "array": [500000, 1999000]
     },
     {
         "_id": 4,
-        "name": "2.000.000VNĐ to 4.999.000VNĐ",
-        "array": [2000, 4999]
+        "name": "2.000.000VNĐ đến 4.999.000VNĐ",
+        "array": [2000000, 4999000]
     },
     {
         "_id": 5,
         "name": "Hơn 5.000.000VNĐ",
-        "array": [5000, 1500000]
+        "array": [5000000, 1500000000]
     }
 ]
 
@@ -37,32 +37,194 @@ const price = [
 const category = [
     {
         "_id": 1,
-        "name": "Thời trang"
+        "gender": 1,
+        "group": 1,
+        "name": "Áo Polo Nam"
     },
     {
         "_id": 2,
-        "name": "Phụ kiện"
+        "gender": 1,
+        "group": 1,
+        "name": "Áo Thun Nam"
     },
     {
         "_id": 3,
-        "name": "Điện thoại"
+        "gender": 1,
+        "group": 1,
+        "name": "Áo Sơ Mi Nam"
     },
     {
         "_id": 4,
-        "name": "Máy tính & Laptop"
+        "gender": 1,
+        "group": 1,
+        "name": "Áo Khoác Nam"
     },
     {
         "_id": 5,
-        "name": "Linh kiện điện tử"
+        "gender": 1,
+        "group": 1,
+        "name": "Áo Vest - Blazer Nam"
     },
     {
         "_id": 6,
-        "name": "Thiết bị gia dụng"
+        "gender": 1,
+        "group": 1,
+        "name": "Áo Tank Top Nam"
     },
     {
         "_id": 7,
-        "name": "Sách báo"
-    }
+        "gender": 1,
+        "group": 1,
+        "name": "Áo Len Nam"
+    },
+    {
+        "_id": 8,
+        "gender": 1,
+        "group": 1,
+        "name": "Áo Hoodie - Sweater Nam"
+    },
+
+    {
+        "_id": 9,
+        "gender": 1,
+        "group": 2,
+        "name": "Quần Jean Nam"
+    },
+    {
+        "_id": 10,
+        "gender": 1,
+        "group": 2,
+        "name": "Quần Kaki Nam"
+    },
+    {
+        "_id": 11,
+        "gender": 1,
+        "group": 2,
+        "name": "Quần Vải Nam"
+    },
+    {
+        "_id": 12,
+        "gender": 1,
+        "group": 2,
+        "name": "Quần Short Nam"
+    },
+    {
+        "_id": 13,
+        "gender": 1,
+        "group": 2,
+        "name": "Quần Jogger Nam"
+    },
+
+    {
+        "_id": 14,
+        "gender": 2,
+        "group": 1,
+        "name": "Áo Thun Nữ"
+    },
+    {
+        "_id": 15,
+        "gender": 2,
+        "group": 1,
+        "name": "Áo Sơ Mi Nữ"
+    },
+    {
+        "_id": 16,
+        "gender": 2,
+        "group": 1,
+        "name": "Áo Khoác Nữ"
+    },
+    {
+        "_id": 17,
+        "gender": 2,
+        "group": 1,
+        "name": "Áo Vest - Blazer Nữ"
+    },
+    {
+        "_id": 18,
+        "gender": 2,
+        "group": 1,
+        "name": "Áo Tank Top Nữ"
+    },
+    {
+        "_id": 19,
+        "gender": 2,
+        "group": 1,
+        "name": "Áo Len Nữ"
+    },
+    {
+        "_id": 20,
+        "gender": 2,
+        "group": 1,
+        "name": "Áo Hoodie - Sweater Nữ"
+    },
+
+    {
+        "_id": 21,
+        "gender": 2,
+        "group": 2,
+        "name": "Quần Jean Nữ"
+    },
+    {
+        "_id": 22,
+        "gender": 2,
+        "group": 2,
+        "name": "Quần Kaki Nữ"
+    },
+    {
+        "_id": 23,
+        "gender": 2,
+        "group": 2,
+        "name": "Quần Vải Nữ"
+    },
+    {
+        "_id": 24,
+        "gender": 2,
+        "group": 2,
+        "name": "Quần Short Nữ"
+    },
+    {
+        "_id": 25,
+        "gender": 2,
+        "group": 2,
+        "name": "Quần Jogger Nữ"
+    },
+
+    {
+        "_id": 26,
+        "gender": 3,
+        "group": 1,
+        "name": "Nón Băng Đô"
+    },
+    {
+        "_id": 27,
+        "gender": 3,
+        "group": 1,
+        "name": "Túi - Ví - Balo"
+    },
+    {
+        "_id": 28,
+        "gender": 3,
+        "group": 1,
+        "name": "Khăn"
+    },
+    {
+        "_id": 29,
+        "gender": 3,
+        "group": 1,
+        "name": "Thắt Lưng"
+    },
+    {
+        "_id": 30,
+        "gender": 3,
+        "group": 1,
+        "name": "Vớ - Tất"
+    },
+    {
+        "_id": 31,
+        "gender": 3,
+        "group": 1,
+        "name": "Giày - Dép"
+    },
 ]
 
 
