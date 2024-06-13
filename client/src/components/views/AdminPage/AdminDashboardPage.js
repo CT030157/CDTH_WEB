e70b3aeaeec6b5bb99ddac1703f0e33d04f93694
CT030157 <167410910+CT030157@users.noreply.github.com>
2024-06-13 21:30:23 +0700

    fix++

--- client/src/components/views/AdminPage/AdminDashboardPage.js
+++ client/src/components/views/AdminPage/AdminDashboardPage.js
@@ -1,4 +1,53 @@
 import React from 'react'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  import { Bar } from 'react-chartjs-2';
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+    const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [1,2,3,4,5,6,7],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: [7,6,5,4,3,2,1],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
 
 function AdminDashboardPage(props) {
     
@@ -6,6 +55,9 @@ function AdminDashboardPage(props) {
         <div style={{ width: '80%', margin: '3rem auto' }}>
             <div style={{ textAlign: 'center' }}>
                 <h1>Biểu đồ</h1>
+            </div>
+            <div>
+                <Bar options={options} data={data} />
             </div>
         </div>
     )
